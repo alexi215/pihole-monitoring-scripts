77f3ae760fbf26ee3c26667de1cb582ed58ddf3b
alexi215 <alexi215@users.noreply.github.com>
2025-04-12 17:58:23 -0500

    Minor edits to monitor-pihole

--- bin/monitor-pihole.sh
+++ bin/monitor-pihole.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Load centralized config
+# Load secrets
 source "$(dirname "$0")/../.env"
 
+# Load helper function
+source "$(dirname "$0")/../lib/telegram.sh"
+
+#  Akward: we define a variable, before determining if we need to use it at all.
 DATE_STR=$(date)
 
 # Check Pi-hole status
 if ! systemctl is-active --quiet pihole-FTL; then
     MSG="❌ *ALERT:* Pi-hole *$PIHOLE_HOSTNAME* is *down* at $DATE_STR
 
-Please check service status and logs immediately."
+	Please check service status and logs immediately."
 
-    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
-         -d chat_id="$TELEGRAM_CHAT_ID" \
-         -d text="$MSG" \
-         -d parse_mode="Markdown"
+	send_telegram "$MSG"
 fi
